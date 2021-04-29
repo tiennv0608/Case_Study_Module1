ea69f2f800bbaa1306a09179f88db139d51bc4a0
tiennv0608 <tiennv0608@gmail.com>
2021-04-29 13:40:26 +0700

    css

--- Moving.js
+++ Moving.js
@@ -1,28 +1,28 @@
 function throwDice(player) {
     turn % 2 === 0 ? player = player2 : player = player1;
     turn++;
-    let valueDice = Math.round(Math.random() * 5 + 1);
+    let valueDice = 1
     document.getElementById('display').innerHTML = valueDice;
     player.move += valueDice;
     if (player.move > 28) {
         player.point += 200;
         document.getElementById('point'+player.number).innerHTML = player.point;
-        x = player.move - 28;
-        let place = arr[28 + x - valueDice - 1];
+        indexMove = player.move - 28;
+        let place = arr[28 + indexMove - valueDice - 1];
         document.getElementById('num-' + place.placeId).innerHTML = place.placeName + '<br>' + place.placeValue;
     } else if (player.move - valueDice - 2 >= 0) {
-        x = player.move;
-        let place = arr[x - valueDice - 1];
+        indexMove = player.move;
+        let place = arr[indexMove - valueDice - 1];
         document.getElementById('num-' + place.placeId).innerHTML = place.placeName + '<br>' + place.placeValue;
     } else {
-        x = player.move;
-        let place = arr[x - valueDice - 1]
+        indexMove = player.move;
+        let place = arr[indexMove - valueDice - 1]
         document.getElementById('num-' + place.placeId).innerHTML = place.placeName + '<br>' + place.placeValue;
     }
-    player.move = x;
-    let place = arr[x - 1];
+    player.move = indexMove;
+    let place = arr[indexMove - 1];
     document.getElementById("num-" + place.placeId).innerHTML += player.getPlayerName() + "<br>";
-    if (x - 1 > 0) {
+    if (indexMove - 1 > 0) {
         z = player
         let str = "Do "+z.getPlayerName()+" want to purchase " + place.placeName + " with " + place.placeValue + "<br>";
         str += "<button type='button' onclick='accept(z)' id='yes'>Yes</button><button type='button' onclick='refuse()'>No</button>"
